19beb0f1ae0f702196295b2e0205ab12f9226546
ndevreeze <nico@ndv.nl>
2021-03-24 14:55:05 +0100

    Bump to 0.2.0-SNAPSHOT
    Wrt setting :inorder true

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ndevreeze/cmdline "0.1.2"
+(defproject ndevreeze/cmdline "0.2.0-SNAPSHOT"
   :description "Command line and script functions"
   :url "https://github.com/ndevreeze/cmdline"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
