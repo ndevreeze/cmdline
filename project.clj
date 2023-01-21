1f8d4a601430a55e22418fd719e0e382ac7bb0f9
ndevreeze <nico@ndv.nl>
2023-01-21 20:14:40 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.cli "1.0.194"]
-                 [clj-commons/fs "1.6.307"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.cli "1.0.214"]
+                 [clj-commons/fs "1.6.310"]]
 
   :target-path "target/%s"
 
   :resource-paths ["resources"]
   
-  :profiles {:dev {:dependencies [[midje "1.9.9"]
-                                  [clj-commons/pomegranate "1.2.0"]]}}
+  :profiles {:dev {:dependencies [[midje "1.10.9"]
+                                  [clj-commons/pomegranate "1.2.1"]]}}
 
   :repl-options {:init-ns ndevreeze.cmdline}
 
