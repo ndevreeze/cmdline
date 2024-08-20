37ad0c64de088db93149ce9da812408c93508051
ndevreeze <nico@ndv.nl>
2024-08-20 20:52:15 +0200

    Bump library version(s) wrt lein ancient 2024-08-20

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   
-  :dependencies [[org.clojure/clojure "1.11.3"]
+  :dependencies [[org.clojure/clojure "1.11.4"]
                  [org.clojure/tools.cli "1.1.230"]
                  [clj-commons/fs "1.6.311"]
 
